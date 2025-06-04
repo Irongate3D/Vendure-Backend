460e1c3d642699658227d4d099198312c219d4dd
Vincent4364 <103389512+Vincent4364@users.noreply.github.com>
2025-06-04 13:46:24 +1200

    Initial commit

---
+++ src/vendure-config.ts
@@ -0,0 +1,95 @@
+import {
+  dummyPaymentHandler,
+  DefaultJobQueuePlugin,
+  DefaultSchedulerPlugin,
+  DefaultSearchPlugin,
+  VendureConfig,
+} from '@vendure/core';
+import { defaultEmailHandlers, EmailPlugin, FileBasedTemplateLoader } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import { GraphiqlPlugin } from '@vendure/graphiql-plugin';
+import { StripePlugin } from '@vendure/payments-plugin/package/stripe';
+import 'dotenv/config';
+import path from 'path';
+
+const IS_DEV = process.env.APP_ENV === 'dev';
+const serverPort = +process.env.PORT || 3000;
+
+export const config: VendureConfig = {
+  apiOptions: {
+    port: serverPort,
+    adminApiPath: 'admin-api',
+    shopApiPath: 'shop-api',
+    ...(IS_DEV
+      ? {
+          adminApiDebug: true,
+          shopApiDebug: true,
+        }
+      : {}),
+  },
+  authOptions: {
+    tokenMethod: ['bearer', 'cookie'],
+    superadminCredentials: {
+      identifier: process.env.SUPERADMIN_USERNAME!,
+      password: process.env.SUPERADMIN_PASSWORD!,
+    },
+    cookieOptions: {
+      secret: process.env.COOKIE_SECRET!,
+    },
+  },
+  dbConnectionOptions: {
+    type: 'postgres',
+    synchronize: true,
+    migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
+    logging: false,
+    database: process.env.DB_NAME,
+    schema: process.env.DB_SCHEMA,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [
+      dummyPaymentHandler,
+    ],
+  },
+  customFields: {},
+  plugins: [
+    GraphiqlPlugin.init(),
+    AssetServerPlugin.init({
+      route: 'assets',
+      assetUploadDir: path.join(__dirname, '../static/assets'),
+      assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets/',
+    }),
+    DefaultSchedulerPlugin.init(),
+    DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
+    DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
+    EmailPlugin.init({
+      devMode: true,
+      outputPath: path.join(__dirname, '../static/email/test-emails'),
+      route: 'mailbox',
+      handlers: defaultEmailHandlers,
+      templateLoader: new FileBasedTemplateLoader(
+        path.join(__dirname, '../static/email/templates'),
+      ),
+      globalTemplateVars: {
+        fromAddress: '"example" <noreply@example.com>',
+        verifyEmailAddressUrl: 'http://localhost:8080/verify',
+        passwordResetUrl: 'http://localhost:8080/password-reset',
+        changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change',
+      },
+    }),
+    AdminUiPlugin.init({
+      route: 'admin',
+      port: serverPort + 2,
+      adminUiConfig: {
+        apiPort: serverPort,
+      },
+    }),
+    StripePlugin.init({
+      storeCustomersInStripe: true,
+    }), // ✅ Correct usage
+  ],
+};
